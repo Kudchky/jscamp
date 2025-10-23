0e4641bbf9396fd58ea17c3fc079ba6785cb24cd
Pol Castillo <rpcastilloy@gmail.com>
2025-10-22 22:38:39 -0500

    termine class carrito

--- 01-javascript/01-exercise-fundamentals/02-carrito-compras/entidades/Carrito.js
+++ 01-javascript/01-exercise-fundamentals/02-carrito-compras/entidades/Carrito.js
@@ -3,8 +3,6 @@ import { buscarProducto, verificarStock } from "../datos/productosData";
 class Carrito {
     constructor() {
         this._items = [];
-        this._total = 0;
-        this._cantidadItems = this._items.length;
     }
 
     itemExiste(productoId) {
@@ -40,10 +38,63 @@ class Carrito {
             const producto = buscarProducto(productoId);
             producto.stock += item.cantidad;
 
-            this._items = this._items.filter(el => el.productoId !== productoId);
+            this._items = this._items.filter(
+                (el) => el.productoId !== productoId
+            );
             return true;
         }
 
         return false;
+    }
+
+    actualizarCantidad(productoId, nuevaCantidad) {
+        if (nuevaCantidad <= 0) {
+            return false;
+        }
+
+        const item = this.itemExiste(productoId);
+
+        if (item) {
+            const producto = buscarProducto(productoId);
+
+            if (producto.stock + item.cantidad >= nuevaCantidad) {
+                producto.stock = producto.stock + item.cantidad - nuevaCantidad;
+                item.cantidad = nuevaCantidad;
+                return true;
+            }
+            return false;
+        }
+
+        return false;
+    }
+
+    calcularTotal() {
+        return this._items.reduce((acc, val) => {
+            const producto = buscarProducto(val.productoId);
+            return acc + producto.price * val.cantidad;
+        }, 0);
+    }
+
+    verCarrito() {
+        return this._items.map((item) => {
+            const producto = buscarProducto(item.productoId);
+            return {
+                ...item,
+                nombre: producto?.name || "Producto no encontrado",
+                precioUnitario: producto?.price || 0,
+                subtotal: (producto?.price || 0) * item.cantidad,
+            };
+        });
+    }
+
+    vaciarCarrito() {
+        this._items.forEach((item) => {
+            const producto = buscarProducto(item.productoId);
+            if (producto) {
+                producto.stock += item.cantidad;
+            }
+        });
+        this._items = [];
+        return true;
     }
 }
